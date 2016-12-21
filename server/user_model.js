c65d358344d466ca5f4bc507e6274bf788fc70f3
Akash Trivedi <akash@citizencode.io>
2016-12-20 20:22:26 -0800

    Fixed typo in user model

--- server/user_model.js
+++ server/user_model.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost/test';
-mongoose.connect(mongodUri);
+mongoose.connect(mongodbUri);
 
 
 var Schema = mongoose.Schema;
